c04533d31fd241a83b88ebbe4d4a643f856edf4a
coisacruel <115575302+lealvivi@users.noreply.github.com>
2024-09-18 08:53:00 -0300

    Update TStaticList.c

--- TStaticList.c
+++ TStaticList.c
@@ -1,39 +1,42 @@
 #include "TStaticList.h"
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 5
-
-struct _list
-{
+struct _list{
+    unsigned int MAX;
     unsigned int qty;
-    int data[MAX];
+    int *data;
 };
 
-TStaticList *SList_create()
-{
-    TStaticList *list = malloc(sizeof(TStaticList));
-    if (list != NULL)
+TStaticList* SList_create(unsigned int size) {
+    TStaticList* list = malloc(sizeof(TStaticList));
+    if(list!=NULL){
+        list->data = malloc(sizeof(int)*size);
+        if(list->data==NULL){
+            free(list);
+            return NULL;
+        }
         list->qty = 0;
+        list->MAX = size;
+    }
     return list;
 }
 
-bool SList_insert_end(TStaticList *list, int info)
-{
-    // Checar se tem espaço
-    if (SList_is_full(list))
+bool SList_insert_end(TStaticList* list, int info){
+    //Checar se tem espaço
+    if(SList_is_full(list))
         return false;
-    list->data[list->qty++] = info;
+    list->data[list->qty] = info;
+    list->qty++;
     return true;
 }
 
-bool SList_is_full(TStaticList *list)
-{
-    return list->qty == MAX;
+bool SList_is_full(TStaticList* list){
+    return list->qty == list->MAX;
 }
 
-void SList_print(TStaticList *list)
-{
-    for (unsigned int i = 0; i < list->qty; i++)
+
+void SList_print(TStaticList* list){
+    for(unsigned int i=0; i<list->qty; i++)
         printf("%d, ", list->data[i]);
     putchar('\n');
 }
